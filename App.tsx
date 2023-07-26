a1572179b8a3682e9eadfd9243119fca82ccb6f1
nazam1998 <nazamfr1998@gmail.com>
2023-07-26 16:51:51 +0500

    added basic navigation with deeplink

--- App.tsx
+++ App.tsx
@@ -1,22 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import {
+  LinkingOptions,
+} from "@react-navigation/native";
+import * as Linking from "expo-linking";
 
-export default function App() {
-  
+const linking: LinkingOptions<any> = {
+  prefixes: [Linking.createURL("/")],
+  config: {
+    screens: {
+      DetailsScreen: "details/:itemId/:otherParam",
+    },
+  },
+};
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const linking: LinkingOptions<any> = {
+    prefixes: [Linking.createURL("/")],
+    config: {
+      screens: {
+        DetailsScreen: "details/:itemId/:otherParam",
+      },
+    },
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
