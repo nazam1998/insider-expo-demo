dcce31e5c0435e69ed0042d9913a2891b87ad0d1
nazam1998 <nazamfr1998@gmail.com>
2023-07-26 21:19:28 +0500

    cleanup code

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
+import * as React from "react";
 import {
-  LinkingOptions,
+  NavigationContainer,
 } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import { LinkingOptions } from "@react-navigation/native";
 import * as Linking from "expo-linking";
 
-const linking: LinkingOptions<any> = {
-  prefixes: [Linking.createURL("/")],
-  config: {
-    screens: {
-      DetailsScreen: "details/:itemId/:otherParam",
-    },
-  },
-};
 
 const Stack = createNativeStackNavigator();
 
